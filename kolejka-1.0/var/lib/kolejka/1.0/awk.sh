086cee5d3bbdd4f7353d0779016adac2156ee974
root <root@centos.local>
2016-05-13 00:38:35 +0200

    cleaned csv output formatting

--- kolejka-1.0/var/lib/kolejka/1.0/awk.sh
+++ kolejka-1.0/var/lib/kolejka/1.0/awk.sh
@@ -31,6 +31,6 @@ fi
 ########### main loop
 
 #convert html to CSV (semicolon separated)
-grep -e 'bold">.*<\/span>' $dst_path_html -e '<td style="font-weight: bold">' -e '<td>' | awk 'match($0,/bold">.*<\/span>/) {print "LOC:" substr($0,RSTART+6,RLENGTH-13)}; match($0,/Ostatnia aktualizacja .*<\/p>/) {print "TIMESTAMP" substr($0,RSTART+21,RLENGTH-25)}; match($0,/td>.*<\/td/) {print substr($0,RSTART+3,RLENGTH-7)};' | awk 'BEGIN {}; /LOC/ {loc=$0; next}; /TIMESTAMP/ {time=$0;next}{line=loc ";" time ";" $0; getline; line=line ";" $0; getline;line=line ";" $0; getline;line=line ";" $0; print line; next; next; next; next}' >> $dst_path_csv 
+grep -e 'bold">.*<\/span>' $dst_path_html -e '<td style="font-weight: bold">' -e '<td>' | awk 'match($0,/bold">.*<\/span>/) {print "LOC:" substr($0,RSTART+6,RLENGTH-13)}; match($0,/Ostatnia aktualizacja .*<\/p>/) {print "TIMESTAMP" substr($0,RSTART+21,RLENGTH-25)}; match($0,/td>.*<\/td/) {print substr($0,RSTART+3,RLENGTH-7)};' | awk 'BEGIN {}; /LOC/ {loc=$0; next}; /TIMESTAMP/ {time=$0;next}{line=loc ";" time ";" $0; getline; line=line ";" $0; getline;line=line ";" $0; getline;line=line ";" $0; print line; next; next; next; next}' | sed 's/TIMESTAMP //g' | sed 's/^ //' >> $dst_path_csv
 
 rm $dst_path_html
